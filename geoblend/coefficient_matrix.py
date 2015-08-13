4041f08ad4c6e9ac49a9b5ade83a915ca1b85667
Amit Kapadia <amit@planet.com>
2015-08-13 10:56:16 -0700

    generate the coefficient matrix from an arbitrary image mask

--- geoblend/coefficient_matrix.py
+++ geoblend/coefficient_matrix.py
@@ -7,6 +7,98 @@ from scipy import sparse
 from scipy.ndimage import convolve
 
 
+def boundary_from_mask(mask):
+    """
+    Find the boundary pixels for a given mask. Boundaries lie
+    outside the data region. This function returns a boundary
+    image (same shape as mask), where the values along the boundaries
+    correspond to the number of 4-connected neighbors that the
+    boundary pixel intersects with the data region.
+    
+    :param mask:
+        ndarray representing the region of valid pixels in an image.
+    """
+    
+    # Structuring element to select the 4-connected neighbors
+    # from a pixel. This is crafted to return the number of
+    # 4-connected pixels intersecting the data region.
+    selem = np.array([
+        [0, 1, 0],
+        [1, -4, 1],
+        [0, 1, 0]
+    ])
+    
+    # TODO: Using the Poission kernal from pyamg can also be done. The sign
+    #       is flipped, and requires a bit more memory to store the sparse
+    #       matrix representation of the kernal.
+    boundaries = convolve(mask, selem)
+    boundaries[boundaries < 0] = 0
+    
+    return boundaries
+
+
+def matrix_from_mask(mask):
+    """
+    Creates the coefficient matrix corresponding to the
+    coefficients of the linearized Poisson problem.
+    
+    :param mask:
+        ndarray representing the region of valid pixels in an image.
+    """
+    
+    height, width = mask.shape
+    n = width * height
+    
+    m = mask.ravel()
+    indices = np.nonzero(m)
+    
+    # The coefficient matrix is composed of 5 diagonals.
+    
+    # Coefficients correspond to regions of valid data as designated
+    # by the mask, and boundaries as selected by 4-connected neighbors
+    
+    #
+    # Center diagonal
+    #
+    c = np.zeros(n)
+    
+    # The coefficient 4 corresponds to regions of valid data
+    # where the index corresponds to the raveled view of the mask.
+    c[indices] = 4
+    b = boundary_from_mask(mask).ravel()
+    c += b
+    
+    #
+    # Upper diagonal
+    #
+    u = np.zeros(n - 1)
+    u[indices] = 1
+    u = -1 * np.logical_or(u, np.roll(u, -1))
+    
+    #
+    # Lower diagonal
+    #
+    l = np.zeros(n - 1)
+    l[indices] = 1
+    l = -1 * np.logical_or(l, np.roll(l, -1))
+    
+    #
+    # Upper upper diagonal (it's like RMNP, except with less chaos)
+    #
+    uu = np.zeros(n - width)
+    uu[indices] = -1
+    uu[indices[0] - width] = -1
+    
+    #
+    # Lower lower diagonal
+    #
+    ll = np.zeros(n - width)
+    ll[indices] = -1
+    ll[indices[0] - width] = -1
+    
+    return sparse.diags((ll, l, c, u, uu), [-1 * width, -1, 0, 1, width], format='csr')
+
+
 def get_coefficient_matrix_size(mask):
     """
     Get the number of terms required for solving the Poisson equation
