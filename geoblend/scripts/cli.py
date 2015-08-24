c270c793e13c109ae5af07f6e01672262f188d63
Amit Kapadia <amit@planet.com>
2015-08-24 16:39:20 -0700

    use blend func in cli

--- geoblend/scripts/cli.py
+++ geoblend/scripts/cli.py
@@ -1,47 +1,33 @@
 
 import click
 import numpy as np
+import rasterio as rio
 from scipy import sparse
-from scipy.ndimage import convolve
 
 import pyamg
 from pyamg.relaxation.smoothing import change_smoothers
-import rasterio as rio
 from skimage.morphology import binary_erosion, square
 
-import geoblend
+from geoblend import blend
 from geoblend.coefficients import matrix_from_mask
-from geoblend.coefficient_matrix import create_multilevel_solver
-from geoblend.b import b
-from geoblend.blend import load_levels
+from geoblend.solver import create_multilevel_solver, load_multilevel_solver
+
+
+def prepare_mask(mask):
+    """
+    Temporary function to prepare a mask band for experiments.
+    """
+
+    indices = np.nonzero(mask)
+    mask[indices] = 1
+
+    selem = square(4)
+    return binary_erosion(mask, selem).astype(np.uint8)
 
 
 @click.group()
 def geoblend():
     pass
-
-
-def create_vector(mask, source, reference):
-
-    indices = np.nonzero(mask)
-    
-    selem = np.array([
-        [0, -1, 0],
-        [-1, 4, -1],
-        [0, -1, 0]
-    ])
-
-    m = convolve(mask.astype(np.float), selem)
-    m[m < 0] = 0
-    m[m > 0] = 1
-    bindices = np.nonzero(m)
-
-    shape = source.shape
-    operator = pyamg.gallery.poisson(shape)
-    field = (operator * source.ravel()).reshape(shape)
-    field[bindices] = reference[bindices]
-
-    return field[indices]
 
 
 @click.command('poisson')
@@ -61,17 +47,12 @@ def poisson(srcpath, refpath, dstpath, matrix):
             
             profile = src.profile
             
-            # For now the mask should be boolean
             mask = src.read(4)
-            indices = np.nonzero(mask)
-            mask[indices] = 1
-            mask = mask.astype(np.uint8)
-            selem = square(4)
-            mask = binary_erosion(mask, selem).astype(np.uint8)
+            mask = prepare_mask(mask)
             indices = np.nonzero(mask)
             
             if matrix:
-                levels = load_levels(matrix)
+                levels = load_multilevel_solver(matrix)
                 ml = pyamg.multilevel.multilevel_solver(levels, coarse_solver='pinv2')
                 change_smoothers(ml, 'gauss_seidel', 'gauss_seidel')
             else:
@@ -90,17 +71,9 @@ def poisson(srcpath, refpath, dstpath, matrix):
 
                     source = src.read(bidx)
                     reference = ref.read(bidx)
-                    
-                    field = (operator * source.ravel()).reshape(shape).astype(np.int32)
-                    vector = create_vector(mask, source, reference).astype('float64')
 
-                    x0 = source[indices].astype('float64')
-                    pixels = np.round(ml.solve(b=vector, x0=x0, tol=1e-16))
-                    np.clip(pixels, 0, 4095, pixels)
-
-                    reference[indices] = pixels
-
-                    dst.write_band(bidx, reference)
+                    arr = blend(source, reference, mask, operator, ml)
+                    dst.write_band(bidx, arr)
 
 
 @click.command('create-solver')
@@ -108,27 +81,21 @@ def poisson(srcpath, refpath, dstpath, matrix):
 @click.argument('dstpath', type=click.Path(exists=False))
 def create_solver(srcpath, dstpath):
     """
-    Create a multi-level solver and cache to disk.
+    Create a multi-level solver and save to disk.
     """
-    
+
     with rio.drivers():
         with rio.open(srcpath) as src:
 
             mask = src.read(4)
+            mask = prepare_mask(mask)
             indices = np.nonzero(mask)
-            mask[indices] = 1
-            mask = mask.astype(np.uint8)
-            
-            print mask
-            selem = square(4)
-            mask = binary_erosion(mask, selem).astype(np.uint8)
-            
-            print mask
-            
+
             data, row, col, height, width = matrix_from_mask(mask)
             mat = sparse.csr_matrix((data, (row, col)), shape=(height, width))
 
     create_multilevel_solver(dstpath, mat)
+
 
 geoblend.add_command(poisson)
 geoblend.add_command(create_solver)
