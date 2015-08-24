096155e9b3f8fdad3927ded921768155e405b1b8
Amit Kapadia <amit@planet.com>
2015-08-24 16:37:23 -0700

    func to load levels on disk

--- geoblend/solver.py
+++ geoblend/solver.py
@@ -4,10 +4,10 @@ import numpy as np
 from scipy import sparse
 
 
-def save_levels(filename, levels):
+def save_multilevel_solver(filename, levels):
     """
     Save levels from a PyAMG multilevel solver
-    
+
     :param filename:
     :param levels:
         PyAMG multilevel solver
@@ -42,6 +42,47 @@ def save_levels(filename, levels):
     np.savez(filename, levels=output)
 
 
+def load_multilevel_solver(filename):
+    """
+    Load a multilevel solver from disk.
+
+    :param filename:
+    """
+
+    class level:
+
+        def __init__(self):
+            pass
+
+
+    def load_level(item):
+
+        l = level()
+        for key, value in item.iteritems():
+
+            if key == 'presmoother':
+                continue
+
+            if type(value) is np.ndarray:
+                arr = value
+            else:
+                matrix_func_name = "%s_matrix" % value["format"]
+                matrix_func = getattr(sparse, matrix_func_name)
+                arr = matrix_func((
+                    value["data"],
+                    value["indices"],
+                    value["indptr"]),
+                    shape=value["shape"]
+                )
+
+            setattr(l, key, arr)
+
+        return l
+
+    data = np.load(filename)
+    return map(load_level, data["levels"])
+
+
 def create_multilevel_solver(filename, matrix):
     """
     Precompute a multilevel solver for a given coefficient matrix.
@@ -53,8 +94,9 @@ def create_multilevel_solver(filename, matrix):
     :param matrix:
         CSR sparse matrix
     """
+
     shape = matrix.shape
     b = np.ones((shape[0], 1))
     ml = pyamg.smoothed_aggregation_solver(matrix, b, max_coarse=10)
     
-    save_levels(filename, ml.levels)
+    save_multilevel_solver(filename, ml.levels)
