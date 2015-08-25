adfa6832647981b2fe2c89c3e48cc7cfa10280e9
Amit Kapadia <amit@planet.com>
2015-08-24 17:19:46 -0700

    mask function

--- geoblend/utilities.py
+++ geoblend/utilities.py
@@ -1,6 +1,35 @@
 
+import numpy as np
 import rasterio as rio
 from shapely.geometry.geo import box
+from skimage.morphology import binary_erosion, square
+
+
+def get_mask(srcpath):
+    """
+    Read the mask for a given filepath. It is assumed
+    that the 4th band corresponds to a mask. If a 4th
+    band does not exist, a square mask is generated with
+    border pixels marked out.
+
+    :param srcpath:
+    """
+
+    with rio.drivers():
+        with rio.open(srcpath) as src:
+            count = src.count
+
+            if count < 4:
+                mask = np.ones_like(src.shape)
+                mask[0, :] = 0
+                mask[-1, :] = 0
+                mask[:, 0] = 0
+                mask[:, -1] = 0
+            else:
+                mask = src.read(4)
+                return binary_erosion(mask, square(4)).astype(np.uint8)
+
+    return mask
 
 
 def get_metadata(fpath):
@@ -103,5 +132,5 @@ def get_boundary_window(window, side=None, extrude=1):
         'left': ((y0, y1), (x0, x0 + extrude)),
         'right': ((y0, y1), (x1 - extrude, x1)),
         None: window
-    }[side]    
+    }[side]
     
