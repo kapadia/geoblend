1a251189153fc9a5a0615207a96b55ea4d6c6063
Amit Kapadia <amit@planet.com>
2015-08-31 16:31:50 -0700

    updated coeff benchmark

--- benchmark/benchmark_coefficients.py
+++ benchmark/benchmark_coefficients.py
@@ -2,33 +2,20 @@
 import benchmark
 import numpy as np
 from skimage.morphology import disk
-from geoblend.coeffs import matrix_from_mask_slow
-from geoblend.coeffs_fast import matrix_from_mask_fast
-from geoblend.coeffs_numba import matrix_from_mask_numba
-
-
-# class Benchmark_Coefficients_Python(benchmark.Benchmark):
-#
-#     def setUp(self):
-#         self.mask = np.pad(disk(50), 2, mode='constant')
-#
-#     def test_coefficients(self):
-#         mat = matrix_from_mask_slow(self.mask)
+from geoblend.coefficients import matrix_from_mask as matrix_from_mask_cython
+from geoblend import matrix_from_mask_numba
 
 
 class Benchmark_Coefficients(benchmark.Benchmark):
     
     def setUp(self):
-        self.mask = np.pad(disk(200), 2, mode='constant')
+        self.mask = np.pad(disk(2000), 2, mode='constant')
     
     def test_cython(self):
-        mat = matrix_from_mask_fast(self.mask)
+        mat = matrix_from_mask_cython(self.mask)
     
     def test_numba(self):
         mat = matrix_from_mask_numba(self.mask)
-    
-    # def test_python(self):
-    #     mat = matrix_from_mask_slow(self.mask)
 
 
 
