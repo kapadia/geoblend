b95c3d4d3769b1d2065c33989a474c858f76de67
Amit Kapadia <amit@planet.com>
2015-09-26 11:02:52 -0700

    enable openmp

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 
+import os
 from codecs import open as codecs_open
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 import numpy as np
 
+# TODO: Generalize this. Check that openmp is available.
+os.environ["CC"] = "/usr/local/Cellar/gcc/5.1.0/bin/gcc-5"
 
 # Get the long description from the relevant file
 with codecs_open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
-# Add numpy to include directory for cython compilation
 ext_options = {
-    "include_dirs": [ np.get_include() ]
+    "include_dirs": [ np.get_include() ],
+    "extra_compile_args": [ '-fopenmp' ],
+    "extra_link_args": [ '-fopenmp' ]
 }
 
 extensions = [
