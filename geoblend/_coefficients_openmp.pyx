866592fc79f0fb98571d3fe1bd009452be967753
Amit Kapadia <amit@planet.com>
2015-10-07 17:55:33 -0700

    setup script to handle openmp if available

---
+++ geoblend/_coefficients_openmp.pyx
@@ -0,0 +1,164 @@
+
+import numpy as np
+from scipy.sparse import csr_matrix
+
+cimport numpy as np
+cimport cython
+from cython.parallel cimport prange, threadid
+cimport openmp
+
+
+cdef inline unsigned int[:] count_nonzero_along_axis(char[:, ::1] arr):
+    
+    cdef:
+        unsigned int i, j, count
+        unsigned int height = arr.shape[0]
+        unsigned int width = arr.shape[1]
+        unsigned int[:] row_sum = np.empty(height, dtype=np.uint32)
+    
+    for j in range(height):
+        count = 0
+        for i in range(width):
+            if arr[j, i] != 0:
+                count += 1
+            if i == width - 1:
+                row_sum[j] = count
+
+    return np.asarray(row_sum)
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def matrix_from_mask(char[:, ::1] mask):
+    """
+    Create the coefficient matrix corresponding to the
+    linearized Poisson problem. This function uses a mapping
+    between valid pixels specified by nonzero elements of
+    a mask to construct the matrix.
+
+    :param mask:
+        ndarray (uint8) where nonzero values represent the region
+        of valid pixels in an image.
+    """
+
+
+    cdef:
+
+        unsigned int height = mask.shape[0]
+        unsigned int width = mask.shape[1]
+
+        # Equation index and coefficient index
+        int eidx = 0
+        int cidx = 0
+
+        # Determine the number of coefficients that will be stored
+        # One approach is to convolve with a structuring element that
+        # counts the number of neighbors for each pixel, but this
+        # is costly.
+        #
+        # Another approach is to be generous when allocating the row/column/data
+        # arrays by using the upper bound for the number of coefficients.
+        # For N unknown pixels, there are at most 5 * N coefficients.
+        unsigned int[:] row_sum = count_nonzero_along_axis(mask)
+        unsigned int n = np.sum(row_sum)
+        unsigned int n_coeff = 5 * n
+
+        unsigned int i, j, nj, ni, sj, si, ej, ei, wj, wi, neighbors
+        unsigned int row_north, row_current, row_south
+        int offset
+
+        unsigned int[:] row = np.empty(n_coeff, dtype=np.uint32)
+        unsigned int[:] col = np.empty(n_coeff, dtype=np.uint32)
+        int[:] data = np.empty(n_coeff, dtype=np.int32)
+
+    for j in range(1, height - 1):
+
+        # Keep track of the nonzero counts in the north, current and south rows
+        row_north = 0
+        row_current = 0
+        row_south = 0
+
+        for i in range(1, width - 1):
+
+            # Define indices of 4-connected neighbors
+            nj = <unsigned int>(j - 1)
+            ni = <unsigned int>(i)
+
+            sj = <unsigned int>(j + 1)
+            si = <unsigned int>(i)
+
+            ej = <unsigned int>(j)
+            ei = <unsigned int>(i + 1)
+
+            wj = <unsigned int>(j)
+            wi = <unsigned int>(i - 1)
+
+            if mask[nj, ni] != 0:
+                row_north += 1
+
+            if mask[sj, si] != 0:
+                row_south += 1
+
+            if mask[j, i] == 0:
+                continue
+
+            neighbors = 0
+            row_current += 1
+
+            if mask[nj, ni] != 0:
+
+                neighbors += 1
+
+                offset = row_current - 1
+                offset += row_sum[nj] - row_north + 1
+
+                row[cidx] = eidx
+                col[cidx] = eidx - offset
+                data[cidx] = -4
+
+                cidx += 1
+
+            if mask[sj, si] != 0:
+
+                neighbors += 1
+
+                offset = row_south - 1
+                offset += row_sum[j] - row_current + 1
+
+                row[cidx] = eidx
+                col[cidx] = eidx + offset
+                data[cidx] = -4
+
+                cidx += 1
+
+            if mask[ej, ei] != 0:
+
+                neighbors += 1
+
+                row[cidx] = eidx + 1
+                col[cidx] = eidx
+                data[cidx] = -4
+
+                cidx += 1
+
+            if mask[wj, wi] != 0:
+
+                neighbors += 1
+
+                row[cidx] = eidx - 1
+                col[cidx] = eidx
+                data[cidx] = -4
+
+                cidx += 1
+
+            row[cidx] = eidx
+            col[cidx] = eidx
+            data[cidx] = 2 * neighbors + 8
+        
+            # Increment the equation index
+            cidx += 1
+            eidx += 1
+
+    # Return a slice since the allocation was an approximation
+    return csr_matrix((data[0:cidx], (row[0:cidx], col[0:cidx])), shape=(n, n))
+
--- setup.py
+++ setup.py
@@ -1,23 +1,72 @@
 
+import os
+import shutil
 from codecs import open as codecs_open
 from setuptools import setup, find_packages
-from setuptools.extension import Extension
+from distutils.core import Distribution, Extension
+from distutils.command.build_ext import build_ext
+from distutils.errors import LinkError
+# from setuptools.extension import Extension
 from Cython.Build import cythonize
 import numpy as np
+
+# TODO: Determine how to check for OpenMP
+# os.environ["CC"] = "/usr/local/Cellar/gcc/5.1.0/bin/gcc-5"
+
+
+
+def check_for_openmp():
+    """
+    There does not seem to be a cross platform and standard way to check for
+    OpenMP support. Attempt to compile a test script. Proceed with OpenMP
+    implementation if it works.
+    """
+
+    distribution = Distribution()
+    ext_options = {
+        'extra_compile_args': ['-fopenmp'],
+        'extra_link_args': ['-fopenmp']
+    }
+
+    extensions = [
+        Extension('geoblend.openmp', ['geoblend/openmp.pyx'], **ext_options)
+    ]
+
+    build_extension = build_ext(distribution)
+    build_extension.finalize_options()
+    build_extension.extensions = cythonize(extensions, force=True)
+    build_extension.run()
+
+
+ext_options = {
+    'include_dirs': [ np.get_include() ]
+}
+extensions = [
+    Extension('geoblend.vector', ['geoblend/vector.pyx'], **ext_options)
+]
+
+pkg_dir = os.path.dirname(os.path.realpath(__file__))
+dst = os.path.join(pkg_dir, 'geoblend', 'coefficients.pyx')
+try:
+
+    check_for_openmp()
+    ext_options['extra_compile_args'] = ['-fopenmp']
+    ext_options['extra_link_args'] = ['-fopenmp']
+    src = os.path.join(pkg_dir, 'geoblend', '_coefficients_openmp.pyx')
+except LinkError:
+    src = os.path.join(pkg_dir, 'geoblend', '_coefficients.pyx')
+
+shutil.copy(src, dst)
+
+extensions.append(
+    Extension('geoblend.coefficients', ['geoblend/coefficients.pyx'], **ext_options),
+)
 
 
 # Get the long description from the relevant file
 with codecs_open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
-ext_options = {
-    "include_dirs": [ np.get_include() ]
-}
-
-extensions = [
-    Extension('geoblend.coefficients', ['geoblend/coefficients.pyx'], **ext_options),
-    Extension('geoblend.vector', ['geoblend/vector.pyx'], **ext_options)
-]
 
 setup(name='geoblend',
       version='0.1.0',
@@ -31,7 +80,7 @@ setup(name='geoblend',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
-      ext_modules=cythonize(extensions),
+      ext_modules=cythonize(extensions, force=True),
       zip_safe=False,
       install_requires=[
           'click',
