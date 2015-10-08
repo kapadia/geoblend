3e7dff93cc24de1f7e34b7bafef0dae640dd2693
Amit Kapadia <amit@planet.com>
2015-10-08 13:02:38 -0700

    use thread-local variables

--- geoblend/_coefficients_omp.pyx
+++ geoblend/_coefficients_omp.pyx
@@ -47,11 +47,9 @@ def matrix_from_mask(char[:, ::1] mask):
         unsigned int height = mask.shape[0]
         unsigned int width = mask.shape[1]
 
-        int max_threads = openmp.omp_get_max_threads()
-
         # Equation index and coefficient index
-        unsigned int[:] eidx = np.zeros(max_threads, np.uint32)
-        unsigned int[:] cidx = np.zeros(max_threads, np.uint32)
+        unsigned int eidx = 0
+        unsigned int cidx = 0
 
         # Determine the number of coefficients that will be stored
         # One approach is to convolve with a structuring element that
@@ -62,123 +60,109 @@ def matrix_from_mask(char[:, ::1] mask):
         # arrays by using the upper bound for the number of coefficients.
         # For N unknown pixels, there are at most 5 * N coefficients.
         unsigned int[:] row_sum = count_nonzero_along_axis(mask)
+        unsigned int[:] cum_row_sum = np.cumsum(row_sum, dtype=np.uint32)
         unsigned int n = np.sum(row_sum)
         unsigned int n_coeff = 5 * n
-
-        unsigned int i, j
-
-        unsigned int[:] nj = np.zeros(max_threads, np.uint32)
-        unsigned int[:] ni = np.zeros(max_threads, np.uint32)
-        unsigned int[:] sj = np.zeros(max_threads, np.uint32)
-        unsigned int[:] si = np.zeros(max_threads, np.uint32)
-        unsigned int[:] ej = np.zeros(max_threads, np.uint32)
-        unsigned int[:] ei = np.zeros(max_threads, np.uint32)
-        unsigned int[:] wj = np.zeros(max_threads, np.uint32)
-        unsigned int[:] wi = np.zeros(max_threads, np.uint32)
-
-        unsigned int[:] neighbors = np.zeros(max_threads, np.uint32)
-
-        unsigned int[:] row_north = np.zeros(max_threads, np.uint32)
-        unsigned int[:] row_current = np.zeros(max_threads, np.uint32)
-        unsigned int[:] row_south = np.zeros(max_threads, np.uint32)
 
         unsigned int i, j, nj, ni, sj, si, ej, ei, wj, wi, neighbors
         unsigned int row_north, row_current, row_south
 
-        int[:] offset = np.zeros(max_threads, np.int32)
+        int offset
 
-        unsigned int[:] row = np.empty(n_coeff, dtype=np.uint32)
-        unsigned int[:] col = np.empty(n_coeff, dtype=np.uint32)
-        int[:] data = np.empty(n_coeff, dtype=np.int32)
+        unsigned int[:] row = np.zeros(n_coeff, dtype=np.uint32)
+        unsigned int[:] col = np.zeros(n_coeff, dtype=np.uint32)
+        int[:] data = np.zeros(n_coeff, dtype=np.int32)
 
     for j in prange(1, height - 1, nogil=True):
 
         # Keep track of the nonzero counts in the north, current and south rows
-        row_north[threadid()] = 0
-        row_current[threadid()] = 0
-        row_south[threadid()] = 0
+        row_north = 0
+        row_current = 0
+        row_south = 0
+
+        cidx = 5 * cum_row_sum[j] - 5 * row_sum[j]
+        eidx = cum_row_sum[j] - row_sum[j]
 
         for i in range(1, width - 1):
 
             # Define indices of 4-connected neighbors
-            nj[threadid()] = <unsigned int>(j - 1)
-            ni[threadid()] = <unsigned int>(i)
+            nj = <unsigned int>(j - 1)
+            ni = <unsigned int>(i)
 
-            sj[threadid()] = <unsigned int>(j + 1)
-            si[threadid()] = <unsigned int>(i)
+            sj = <unsigned int>(j + 1)
+            si = <unsigned int>(i)
 
-            ej[threadid()] = <unsigned int>(j)
-            ei[threadid()] = <unsigned int>(i + 1)
+            ej = <unsigned int>(j)
+            ei = <unsigned int>(i + 1)
 
-            wj[threadid()] = <unsigned int>(j)
-            wi[threadid()] = <unsigned int>(i - 1)
+            wj = <unsigned int>(j)
+            wi = <unsigned int>(i - 1)
 
             if mask[nj, ni] != 0:
-                row_north[threadid()] += 1
+                row_north = row_north + 1
 
             if mask[sj, si] != 0:
-                row_south[threadid()] += 1
+                row_south = row_south + 1
 
             if mask[j, i] == 0:
                 continue
 
-            neighbors[threadid()] = 0
-            row_current[threadid()] += 1
+            neighbors = 0
+            row_current = row_current + 1
 
-            if mask[nj[threadid()], ni[threadid()]] != 0:
+            if mask[nj, ni] != 0:
 
-                neighbors[threadid()] += 1
+                neighbors = neighbors + 1
 
-                offset[threadid()] = row_current[threadid()] - 1
-                offset[threadid()] += row_sum[nj[threadid()]] - row_north[threadid()] + 1
-
-                row[cidx] = eidx
-                col[cidx] = eidx - offset[threadid()]
-                data[cidx] = -4
-
-                cidx += 1
-
-            if mask[sj[threadid()], si[threadid()]] != 0:
-
-                neighbors[threadid()] += 1
-
-                offset[threadid()] = row_south[threadid()] - 1
-                offset[threadid()] += row_sum[j] - row_current[threadid()] + 1
+                offset = row_current - 1
+                offset = offset + row_sum[nj] - row_north + 1
 
                 row[cidx] = eidx
-                col[cidx] = eidx + offset[threadid()]
+                col[cidx] = eidx - offset
                 data[cidx] = -4
 
-                cidx += 1
+                cidx = cidx + 1
 
-            if mask[ej[threadid()], ei[threadid()]] != 0:
+            if mask[sj, si] != 0:
 
-                neighbors[threadid()] += 1
+                neighbors = neighbors + 1
+
+                offset = row_south - 1
+                offset = offset + row_sum[j] - row_current + 1
+
+                row[cidx] = eidx
+                col[cidx] = eidx + offset
+                data[cidx] = -4
+
+                cidx = cidx + 1
+
+            if mask[ej, ei] != 0:
+
+                neighbors = neighbors + 1
 
                 row[cidx] = eidx + 1
                 col[cidx] = eidx
                 data[cidx] = -4
 
-                cidx += 1
+                cidx = cidx + 1
 
-            if mask[wj[threadid()], wi[threadid()]] != 0:
+            if mask[wj, wi] != 0:
 
-                neighbors[threadid()] += 1
+                neighbors = neighbors + 1
 
                 row[cidx] = eidx - 1
                 col[cidx] = eidx
                 data[cidx] = -4
 
-                cidx += 1
+                cidx = cidx + 1
 
             row[cidx] = eidx
             col[cidx] = eidx
-            data[cidx] = 2 * neighbors[threadid()] + 8
+            data[cidx] = 2 * neighbors + 8
 
             # Increment the equation index
-            cidx += 1
-            eidx += 1
+            cidx = cidx + 1
+            eidx = eidx + 1
 
-    indices = np.nonzero(data)
-    return csr_matrix((data[indices], (row[indices], col[indices])), shape=(n, n))
+    return csr_matrix((data, (row, col)), shape=(n, n))
 
