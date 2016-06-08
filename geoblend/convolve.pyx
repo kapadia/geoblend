f753f0e635ca6659bb479bb338726c94c5284340
Amit Kapadia <amit@planet.com>
2016-06-08 11:16:28 -0700

    approximate convolution only within threshold

--- geoblend/convolve.pyx
+++ geoblend/convolve.pyx
@@ -5,7 +5,7 @@ cimport numpy as np
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def convolve_mask_aware(unsigned short[:, ::1] arr, char[:, ::1] mask):
+def convolve_mask_aware(unsigned short[:, ::1] arr, char[:, ::1] mask, double threshold = 0.0):
     """
     Convolve a 2D array with a 3x3 structuring element. The convolution operates only over
     regions specified by the mask. The following kernel is used:
@@ -20,16 +20,15 @@ def convolve_mask_aware(unsigned short[:, ::1] arr, char[:, ::1] mask):
         ndarray where nonzero values represent the region
         of valid pixels in an image. The mask should be
         typed to uint8.
-
-    .. todo:: Allow a kernel to be specified.
-    .. todo:: Allow rules along mask/array boundaries.
+    :param threshold:
+        Difference threshold to turn off the convolution approximation.
     """
 
     cdef int height = mask.shape[0]
     cdef int width = mask.shape[1]
 
     cdef int i, j, nj, ni, sj, si, ej, ei, wj, wi, neighbors
-    cdef double x
+    cdef double x, s, d
     cdef double[:, :] img = np.zeros((height, width), dtype=np.float64)
 
     assert arr.shape[0] == height
@@ -42,6 +41,7 @@ def convolve_mask_aware(unsigned short[:, ::1] arr, char[:, ::1] mask):
                 continue
 
             x = 0.0
+            s = <double>arr[j, i]
             neighbors = 0
 
             # Define indices of 4-connected neighbors
@@ -59,22 +59,47 @@ def convolve_mask_aware(unsigned short[:, ::1] arr, char[:, ::1] mask):
 
             # 1. Check that the neighbor index is within image bounds
             # 2. Check that the neighbor index is within the mask
-            if (nj >= 0) and (mask[nj, ni] != 0):          
-                neighbors += 1
-                x += (-1.0 * <double>arr[nj, ni])
+
+            if (nj >= 0):
+                if (mask[nj, ni] != 0):
+                    neighbors += 1
+                    x += (-1.0 * <double>arr[nj, ni])
+                else:
+                    d = np.abs(s - <double>arr[nj, ni])
+                    if (d < threshold):
+                        neighbors += 1
+                        x += (-1.0 * <double>arr[nj, ni])
             
-            if (sj < height) and (mask[sj, si] != 0):
-                neighbors += 1
-                x += (-1.0 * <double>arr[sj, si])
+            if  (sj < height):
+                if (mask[sj, si] != 0):
+                    neighbors += 1
+                    x += (-1.0 * <double>arr[sj, si])
+                else:
+                    d = np.abs(s - <double>arr[sj, si])
+                    if (d < threshold):
+                        neighbors += 1
+                        x += (-1.0 * <double>arr[sj, si])
             
-            if (ei < width) and (mask[ej, ei] != 0):
-                neighbors += 1
-                x += (-1.0 * <double>arr[ej, ei])
+            if (ei < width):
+                if (mask[ej, ei] != 0):
+                    neighbors += 1
+                    x += (-1.0 * <double>arr[ej, ei])
+                else:
+                    d = np.abs(s - <double>arr[ej, ei])
+                    if (d < threshold):
+                        neighbors += 1
+                        x += (-1.0 * <double>arr[ej, ei])
             
-            if (wi >= 0) and (mask[wj, wi] != 0):
-                neighbors += 1
-                x += (-1.0 * <double>arr[wj, wi])
-            
+            if (wi >= 0):
+                if (mask[wj, wi] != 0):
+                    neighbors += 1
+                    x += (-1.0 * <double>arr[wj, wi])
+                else:
+                    d = np.abs(s - <double>arr[wj, wi])
+                    if (d < threshold):
+                        neighbors += 1
+                        x += (-1.0 * <double>arr[wj, wi])
+
             x += (<double>neighbors * <double>arr[j, i])
             img[j, i] = x
 
